0b430900a9a81d9d0c2795845e835abf26453286
Dember1998 <demberneptalyramosramirez@yahoo.es>
2018-09-09 18:44:06 -0600

    se agrego comentarios a histori.service

--- src/app/servicios/history.service.ts
+++ src/app/servicios/history.service.ts
@@ -8,15 +8,25 @@ import { Subject, Observable } from 'rxjs';
 export class HistoryService {
 
   constructor() { }
-
+  /**
+   * emisor de eventos del historial
+   */
   private history$ = new Subject<string[]>();
+  /**
+   * aqui se guardara el historial
+   */
   private history: string[] = [];
-
+  /**
+   * agrega una operacion al historial
+   * @param op operacion a guardar
+   */
   addHistory(op: string) {
     this.history.push(op);
     this.history$.next(this.history);
   }
-
+  /**
+   * devuelve todo el historial
+   */
   getAllHistory(): Observable<string[]> {
     return this.history$.asObservable();
   }

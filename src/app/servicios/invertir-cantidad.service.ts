ad1ae7bbb3b448f940dc1dae34fecf53b03cfcf4
Dember1998 <demberneptalyramosramirez@yahoo.es>
2018-10-16 11:36:15 -0600

    se reprogramo el servicio invertir-cantidad
    se elimino la dependencia de el servicio cantidadActual
    
    se elimino los metodos:
    -reduceSignos
    -removeCursor
    -reduceMultiples
    -invertirEsto
    -get calcTextCursor
    -getCantidadActual
    -escapeStrToRegExpr
    
    reduciendo en gran cantidad las lineas de programacion

--- src/app/servicios/invertir-cantidad.service.ts
+++ src/app/servicios/invertir-cantidad.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@angular/core';
-import { isSigno, isNumber } from '../funciones';
-import { CalcService } from './calc.service';
-import { TextCenterService, ITexCenter } from './text-center.service';
-import { TexCursor } from '../calc-class';
 import { TextCursorService } from './text-cursor.service';
 
 @Injectable({
@@ -10,96 +6,77 @@ import { TextCursorService } from './text-cursor.service';
 })
 export class InvertirCantidadService {
 
-  textCursor: TexCursor;
-  cantidadActual: ITexCenter;
+  textWithCursor: string;
+
+  cursorAnDigit = '((\\d*\\|\\d+)|(\\d+\\|\\d*))';
+
+  reg = {
+    cursorBetweenSing: /(?<=[+-])\|[+-]\d+/,
+    oneSignRegex: new RegExp('(?<=[^+-]\\)?)[+-]' + this.cursorAnDigit),
+    twoSignRegex: new RegExp('(?<=[^+-][+-])[+-]' + this.cursorAnDigit),
+    numberOrSignStart: new RegExp('(^\\|[+-]\\d+)|(^(?:[+-]|^)' + this.cursorAnDigit + ')'),
+  };
 
   constructor(
-    private textCenterService: TextCenterService,
     private textCursorService: TextCursorService
   ) {
-
-    this.textCenterService
-      .getTextCenter$()
-      .subscribe(text => this.cantidadActual = text);
-
-    this.textCursorService
-      .getTextCursor$()
-      .subscribe(textCursor => this.textCursor = textCursor);
+    this.textCursorService.getTextCursor$().subscribe(textCursor => {
+      this.textWithCursor = textCursor.start + '|' + textCursor.end;
+    });
   }
 
-  // convierte positivo a negativo y biseversa la cantidad actual
-  invertir(): string {
-    let txtCenter = this.getCantidadActual();
-    let calcText = this.calcTextCursor;
 
-    calcText = calcText.replace(txtCenter.match(/(\-?)(\d*)\|(\d*)/)[0], '|');
-    txtCenter = this.invertirEsto(txtCenter);
-    calcText = calcText.replace('|', txtCenter);
-    calcText = this.reduceSignos(calcText);
+  replace(val: any, action = '') {
+    val = val.replace('|', '');
 
-    return this.removeCursor(calcText);
-  }
-
-  /**elimina signos repetidos */
-  reduceSignos(str: string): string {
-    for (let signo of '+-/*') {
-      str = this.reduceMultiples(str, signo);
-    }
-    return str;
-  }
-
-  removeCursor(str: string) {
-    let result = Array.from(str).filter(char => char !== '|');
-    return ''.concat(...result);
-  }
-
-  /**Reduce múltiples instancias de un carácter particular que ocurren uno detrás de otro */
-  reduceMultiples(str: string, character: string) {
-    let pattern = new RegExp(`\\${character}{2,}`);
-    return str.replace(pattern, character);
-  }
-
-  invertirEsto(cantidad: string) {
-    if (isNumber(cantidad[0])) {
-      return `-${cantidad}`;
-    } else if (cantidad.startsWith('-')) {
-      return cantidad.replace(/^\-/, '+');
-    } else if (cantidad.startsWith('+')) {
-      return cantidad.replace(/^\+/, '-');
-    }
-  }
-
-  public get calcTextCursor(): string {
-    return this.textCursor.start + '|' + this.textCursor.end;
-  }
-
-  /**
-   * obtiene la cantidad actual con todas las apariciones de signos
-   * a los lados
-   */
-  getCantidadActual(): string {
-    let txtCenter = this.cantidadActual.centerCursor;
-    txtCenter = this.escapeStrToRegExpr(txtCenter);
-
-    let txtCenterCursor_REGEXP = new RegExp(`(\D*)${txtCenter}(\D*)`);
-    let result = txtCenterCursor_REGEXP.exec(this.calcTextCursor);
-
-    return result[0];
-  }
-
-  /**
-   * prepara una cadena para ser utilizada en RegExpr
-   */
-  escapeStrToRegExpr(str: string) {
-    let newStr = '';
-    for (let char of str) {
-      if (isSigno(char) || char.includes('|')) {
-        newStr += '\\' + char;
+    if (action === 'add') {
+      if (Math.sign(val) === -1) {
+        return '-' + val;
       } else {
-        newStr += char;
+        return val[0] + (val * -1);
       }
     }
-    return newStr;
+
+    return +val * -1;
   }
+
+  invert(str: string) {
+
+    if (!str.includes('|')) { throw new Error('no se encontro el cursor'); }
+    let reg = this.reg;
+    // handle 12+-23 to 12+23
+    const handleTwoSign = () => {
+      return str.replace(reg.twoSignRegex, (val) => this.replace(val));
+    };
+
+    // handle 12+23 to 12+-23
+    const handleOneSign = () => {
+
+      // handle 12-5|6 to 12--5|6
+      if (reg.oneSignRegex.test(str)) {
+        return str.replace(reg.oneSignRegex, val => this.replace(val, 'add'));
+      }
+      // handle 2|3 or -2|3
+      // tslint:disable-next-line:one-line
+      else if (reg.numberOrSignStart.test(str)) {
+        return str.replace(reg.numberOrSignStart, (val) => this.replace(val));
+      }
+      // handle 12+|-56 to 12+|56
+      // tslint:disable-next-line:one-line
+      else if (reg.cursorBetweenSing.test(str)) {
+        return str.replace(reg.cursorBetweenSing, (val) => this.replace(val));
+      } else {
+        throw new Error('la accion especificada no existe');
+      }
+
+    };
+
+    return reg.twoSignRegex.test(str) ? handleTwoSign() : handleOneSign();
+  }
+  // convierte positivo a negativo y biseversa la cantidad actual
+  invertir(): string {
+    return this.invert(this.textWithCursor);
+  }
+
 }
 

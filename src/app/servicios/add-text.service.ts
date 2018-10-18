7c45973c6893f087b13b2cad494a0d39e9f9fa57
Dember1998 <demberneptalyramosramirez@yahoo.es>
2018-10-18 15:03:13 -0600

    se utilizaron expresiones regulares para formater el texto en add-text.service
    se eliminaron los metodos:
    -lastChar
    -isNumberLastChar
    -handleConstant
    -handleTrigonometria

--- src/app/servicios/add-text.service.ts
+++ src/app/servicios/add-text.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { TextCursorService } from './text-cursor.service';
 import { TexCursor } from '../calc-class';
-import { isTrigonometria, isNumber, isSigno, isConstant, deleteLast } from '../funciones';
 import { CursorService } from './cursor.service';
 import { FilterSignService } from './filter-sign.service';
 import { CalcSettingService } from '../calc-setting/calc-setting.service';
@@ -19,15 +18,10 @@ export class AddTextService {
     private filterSignService: FilterSignService,
     private settingService: CalcSettingService
   ) {
-    this.cursorService
-      .getPosicionCursor$()
-      .subscribe(posicion => this.posicionCursor = posicion);
-
-    this.textCursorService
-      .getTextCursor$()
-      .subscribe(text => {
-        this.textCursor = text;
-      });
+    this.textCursorService.getTextCursor$().subscribe(text => {
+      this.textCursor = text;
+      this.posicionCursor = text.start.length;
+    });
 
     this.emitText();
   }
@@ -37,89 +31,59 @@ export class AddTextService {
   private posicionCursor = 0;
   private textCursor: TexCursor = { start: '', end: '' };
 
-  /**lastChar of textCursor.start */
-  private get lastChar(): string {
-    return this.textCursor.start.charAt(
-      this.textCursor.start.length - 1
-    );
-  }
-
-  /**es numero el ultimo caracterde this.calcText */
-  private get isNumberLastChar() {
-    return isNumber(this.calcText.charAt(this.calcText.length - 1));
-  }
-
   private setCursor(posicion: number) {
     this.cursorService.setPosicionCursor(posicion + 1);
+  }
+
+  /* se vefificara que que la sintaxis este correcta
+    por ejemplo si se intenta escribir dos signos seguidos 1.. o 1++,
+    en caso de se incorrecta se finalizara la funcion
+    */
+  checkSyntax(tecla) {
+    let noRepeatConfig = this.settingService.getChanges().noRepeat;
+    if (noRepeatConfig) {
+      tecla = this.filterSignService.processkey(tecla);
+      if (tecla === false) { return false; }
+    }
+
+    return true;
   }
 
   /**se reciben la mayoria de las teclas pulsadas y se crea
    * una cadena a partir de esas pulsaciones
    */
   public createText(tecla: any) {
-    /* se vefificara que que la sintaxis este correcta
-    por ejemplo si se intenta escribir dos signos seguidos 1.. o 1++,
-    en caso de se incorrecta se finalizara la funcion
-    */
-    let noRepeatConfig = this.settingService.getChanges().noRepeat;
-    if (noRepeatConfig) {
-      tecla = this.filterSignService.processkey(tecla);
-      if (tecla === false) { return; }
-    }
 
-    if (/pi|e$/.test(this.calcText) && isNumber(tecla)) {
-      this.addTexts('*');
-      this.setCursor(this.posicionCursor + 1);
-    }
+    if (!this.checkSyntax(tecla)) { return; }
 
-    // 12pi = 12*pi
-    if (isConstant(tecla)) {
-      this.handleConstant(tecla);
-    } else if (isTrigonometria(tecla)) {
-      this.handleTrigonometria(tecla);
-    } else {
-      this.addZero(tecla);
-    }
+    this.addTexts(tecla);
+    this.calcText = this.escapeToEval(this.calcText);
+
+    this.calculatePosCursor(tecla);
     this.emitText();
+  }
+
+  escapeToEval(txt: string): string {
+    txt = txt.replace(/(?<=\d)(COS|SEN|TAN)(?!\()/g, '*$1(');
+    txt = txt.replace(/(?<=\d)(pi|e)/g, '*$1');
+    return this.addZero(txt);
+  }
+
+  addZero(txt) {
+    if (!txt.endsWith('.')) {
+      let zeroRegExp = /(?:\d\.(?!\d))|(?:(?<!\d)\.\d)/g;
+      txt = txt.replace(zeroRegExp, x => x.endsWith('.') ? x + '0' : +x + '');
+    }
+
+    return txt;
+  }
+
+  calculatePosCursor(tecla: string) {
+    this.setCursor(this.posicionCursor + tecla.length);
   }
 
   emitText() {
     this.calcText$.next(this.calcText);
-  }
-
-  private handleConstant(tecla: string) {
-    if (this.isNumberLastChar) {
-      this.addTexts(`*${tecla}`);
-      this.setCursor(this.posicionCursor + 2);
-    }
-  }
-
-  private handleTrigonometria(tecla: string) {
-    // 123COS = 123*COS(
-    if (this.isNumberLastChar) {
-      this.addTexts(`*${tecla}(`);
-    } else {
-      this.addTexts(tecla + '(');
-    }
-    if (tecla === 'SQRT') {
-      this.setCursor(this.posicionCursor + 5);
-    } else {
-      this.setCursor(this.posicionCursor + 4);
-    }
-  }
-
-  private addZero(tecla: string) {
-    if (!this.calcText && tecla === '.') {
-      this.addTexts(`0.`);
-    } else if (this.lastChar === '%' && isNumber(tecla)) {
-      this.addTexts(`*${tecla}`); // 4%2 = 4%*2
-    } else if (isSigno(this.lastChar) && tecla === '.') {
-      this.addTexts(`0.`); // 1+. = 1+0.
-    } else if (this.lastChar === '.' && isSigno(tecla)) {
-      this.addTexts(`0${tecla}`); // 1.+ = 1.0+
-    } else {
-      this.addTexts(tecla);
-    }
   }
 
   public setText(text: string) {
@@ -134,17 +98,12 @@ export class AddTextService {
  * crea una cadena con las teclas pulsadas
  * @param tecla tecla pulsada
  */
-  private addTexts(tecla: string): void {
-    // seguir agregando texto cuando el cursor esta en la ultima posicion
+  private addTexts(tecla: string) {
     if (this.calcText.length === this.posicionCursor) {
-      this.calcText += tecla;
-    } else {
-      // agregar  texto cuando el cursor esta dentro
-      // de la cadena ejemplo: 12I4
-      if (this.calcText.length > this.posicionCursor) {
-        this.calcText = this.textCursor.start + tecla + this.textCursor.end;
-      }
+      return this.calcText += tecla;
     }
+
+    this.calcText = this.textCursor.start + tecla + this.textCursor.end;
   }
 
 }
